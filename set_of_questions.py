07b9b859087471f0bd597c7f3121b77c48c1ac4c
Joel <47599765+joelosw@users.noreply.github.com>
2021-12-04 15:26:22 +0200

    added some questions

--- set_of_questions.py
+++ set_of_questions.py
@@ -3,6 +3,9 @@ from question_class import Question
 q1 = Question('logos/facebook.png', 'https://www.facebook.com')
 q2 = Question('logos/microsoft.png', 'https://www.microsoft.com')
 q3 = Question('logos/google.png', 'https://google.com')
-
-
-question_set = set([q1, q2, q3])
+q4 = Question('logos/amazon.png','https://www.amazon.com')
+q5 = Question('logos/PayPal.png','https://www.paypal.com')
+q6 = Question('logos/Bolt.png','https://www.bolt.eu')
+q7 = Question('logos/Tinder.png','https://www.tinder.com')
+q8 = Question('logos/TalTech.png','https://www.taltech.ee')
+question_set = set([q1, q2, q3, q4, q5, q6, q7, q8])
